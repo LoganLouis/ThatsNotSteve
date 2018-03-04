b68b1b56140daa7d1864c985469e4b099b93ce0b
RawrWoofMeow <cass.lindsay@hotmail.com>
2018-03-04 15:54:35 +1030

    Have a thing

---
+++ Connect3Jam/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] animals;
+
+	void Start ()
+    {
+		
+	}
+	
+
+    void Initialize ()
+    {
+        int animalToUse = Random.Range(0, animals.Length);
+        GameObject go = Instantiate(animals[animalToUse], transform.position, Quaternion.identity);
+        go.transform.parent = transform;
+    }
+}
---
+++ Connect3Jam/Assets/Scripts/Board.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    //make grid
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    void Start ()
+    {
+        allTiles = new BackgroundTile[width, height];
+
+        Setup();
+    }
+
+    private void Setup ()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector2 tempPos = new Vector2(x, y);
+                GameObject go = Instantiate(tilePrefab, tempPos, Quaternion.identity);
+                go.transform.parent = transform;
+                go.name = "( " + x + ", " + y + " )";
+            }
+        }
+    }
+}
